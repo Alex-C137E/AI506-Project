5d7e1de1789fa739a3879e18d3655b3e04847e63
Felix den Breejen <FelixdenBreejen@kaist.ac.kr>
2022-05-10 14:54:39 +0900

    Logistic Regression Embedding rewritten

--- cuisine/chef.py
+++ cuisine/chef.py
@@ -20,4 +20,26 @@ def make_binary_data(cookbook, ingredient_list):
             X[idx][ingredient_id] = 1
             
     return X,y
+
+
+def make_embedding_data(cookbook, embedding):
+    # Make the data. X is a matrix with recipe rows and embedding dimension columns, 
+    # where every row is the average of the embeddings in the recipes
+
+    n_recipies = len(cookbook)
+    embedding_dim = len(embedding[0])
+
+    X = np.zeros((n_recipies, embedding_dim))
+    y = np.zeros(n_recipies)
+    for idx, recipe in enumerate(cookbook):
+        embedding_avg = np.zeros(embedding_dim) 
+        ingredient_count = len(recipe['ingredients'])
+        y[idx] = recipe['kitchen_id']
+        for ingredient_id in recipe['ingredients']:
+            embedding_avg += embedding[ingredient_id] / ingredient_count
+            
+        X[idx, :] = embedding_avg
+            
+    return X,y
+    
     
---
+++ cuisine/embedding.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import re
+
+
+def import_embedding(filename, ingredient_list):
+
+    df = pd.read_csv(f'embeddings/{filename}.csv', delimiter=",")
+
+    # Create an embedding dictionary from the file
+    embedding = {}
+    for index, row in df.iterrows():
+        ingredient_id = row[1]
+        embedding_string = row[2]
+        embedding_formatted = re.findall(r'[\d|\.|e|\+|\-]+', embedding_string)
+        embedding_array = np.asarray(embedding_formatted, dtype=float)
+        embedding[ingredient_id] = embedding_array
+
+        
+    add_zero_embedding_for_unknown_ingredients(embedding, ingredient_list)
+        
+    return embedding
+
+
+def add_zero_embedding_for_unknown_ingredients(embedding, ingredient_list):
+
+    embedding_dim = len(embedding[0])
+
+    for ingredient, _ in enumerate(ingredient_list):
+        if ingredient not in embedding:
+            embedding[ingredient] = np.zeros(embedding_dim)
+
+
+def create_random_embedding(ingredient_list, dim):
+
+    embedding = {}
+    for ingredient, _ in enumerate(ingredient_list):
+        embedding[ingredient] = np.random.rand(dim)
+
+    return embedding
